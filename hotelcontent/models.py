c5d712b6d5d5dbf207c7c983bece350fdefce55b
EA2311 <andreyyerzhov@gmail.com>
2021-07-28 18:28:41 +0300

    changed fields

--- hotelcontent/models.py
+++ hotelcontent/models.py
@@ -1,17 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.urls import reverse
 
 User = get_user_model()
 
 
 class Admin(models.Model):
-
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name='Номер телефона', null=True, blank=True)
     address = models.CharField(max_length=255, verbose_name='Адрес', null=True, blank=True)
-
-    #url = models.SlugField(max_length=160, unique=True)
 
     def __str__(self):
         return self.user
@@ -19,38 +15,75 @@ class Admin(models.Model):
 
 class Hotel(models.Model):
     hotel_name = models.CharField(max_length=200)
-    hotel_address = models.CharField(max_length=200)
-    hotel_email = models.CharField(max_length=200)
-    hotel_url = models.CharField(max_length=200)
+    hotel_long = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+    hotel_lat = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+    hotel_email = models.EmailField(max_length=254)
+    hotel_url = models.URLField()
+    admin = models.ForeignKey(User, verbose_name="Администратор", on_delete=models.CASCADE, default=1)
+    hotel_description = models.TextField(default='...')
 
     def __str__(self):
-        return self.hotel_name, self.hotel_address
+        return self.hotel_name
 
 
-class Room_types(models.Model):
-    hotel_type_name = models.CharField(max_length=200)
-    hotel_tupe_description = models.CharField(max_length=200)
+class RoomTypes(models.Model):
+    room_type_name = models.CharField(max_length=200)
+    room_type_description = models.CharField(max_length=200)
+    room_type_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.hotel_type_name
 
 
 class Rooms(models.Model):
     hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
-    room_type = models.ForeignKey(Room_types, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomTypes, on_delete=models.CASCADE)
     room_number = models.IntegerField()
-    room_rate = models.DecimalField(max_digits=5, decimal_places=2)
-    smoking_yn = models.BooleanField()
+
+    # room_price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.hotel, self.room_number,  # self.room_price#
 
 
-class Agent_reservation(models.Model):
+class AgentReservation(models.Model):
     agent_details = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.agent_details
 
-class Booking_status(models.Model):
+
+class BookingStatus(models.Model):
     booking_status_description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.booking_status_description
 
 
 class Bookings(models.Model):
-    agent_reservation = models.ForeignKey(Agent_reservation, on_delete=models.CASCADE)
-    booking_status = models.ForeignKey(Booking_status, on_delete=models.CASCADE)
+    agent_reservation = models.ForeignKey(AgentReservation, on_delete=models.CASCADE)
+    booking_status = models.ForeignKey(BookingStatus, on_delete=models.CASCADE)
     hotels = models.ManyToManyField(Hotel)
-    from_date = models.DateField()
-    until_date = models.DateField(blank=True, null=True)
+    checkin = models.DateField()
+    checkout = models.DateField(blank=True, null=True)
+    rate_price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.agent_reservation, self.hotels, self.checkin, self.checkout
+
+
+class Amenity(models.Model):
+    amenity_name = models.CharField(max_length=200)
+    amenity_price = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+class Coefficient(models.Model):
+    start_date = models.DateField
+    end_date = models.DateField
+    coefficient = models.DecimalField(max_digits=7, decimal_places=2)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+
+
+class RateAmenity(models.Model):
+    room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    amenity = models.ManyToManyField(Amenity)
