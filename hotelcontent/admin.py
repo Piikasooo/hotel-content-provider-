bea8cd4c28801050d65ff3bbcb9b13e4d995b91a
EA2311 <andreyyerzhov@gmail.com>
2021-07-28 19:08:47 +0300

    merge 2, env

--- hotelcontent/admin.py
+++ hotelcontent/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Admin, Hotel, Rooms, Room_types, Agent_reservation, Bookings, Booking_status
+from .models import Admin, Hotel, Rooms, RoomTypes, AgentReservation, Bookings, BookingStatus
 
 
 admin.site.register(Admin)
 admin.site.register(Hotel)
 admin.site.register(Rooms)
-admin.site.register(Room_types)
-admin.site.register(Agent_reservation)
+admin.site.register(RoomTypes)
+admin.site.register(AgentReservation)
 admin.site.register(Bookings)
-admin.site.register(Booking_status)
+admin.site.register(BookingStatus)
