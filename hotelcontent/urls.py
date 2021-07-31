81a32add2e53495c85cba2a4d026d0a02d83732a
MKS <maks1111bm@gmail.com>
2021-07-31 22:42:12 +0300

    feat: add page CreateRoom, CreateCoef; modify CreateRoom (select from possible amenities, select from possible type_room)

--- hotelcontent/urls.py
+++ hotelcontent/urls.py
@@ -1,4 +1,4 @@
-from .views import LoginView, RegistrationView, HomePageView, CreateRoom, AddHotelView, HotelDetailView, CreateAmenityView
+from .views import LoginView, RegistrationView, HomePageView, CreateRoom, AddHotelView, HotelDetailView, CreateAmenityView, CreateCoefficientView
 from django.urls import path
 
 
@@ -8,8 +8,9 @@ urlpatterns = [
     path('homepage/', HomePageView.as_view(), name='homepage'),
     path('add_hotel/', AddHotelView.as_view(), name='add_hotel'),
 
-    path('createroom/<slug:slug>/', CreateRoom.as_view(), name='createroom'),
-    path('createamenity/<slug:slug>/', CreateAmenityView.as_view(), name='createamenity'),
+    path('rooms/<slug:slug>/', CreateRoom.as_view(), name='rooms'),
+    path('amenities/<slug:slug>/', CreateAmenityView.as_view(), name='amenities'),
+    path('coefficient/<slug:slug>/', CreateCoefficientView.as_view(), name='coefficient'),
 
     path('<slug:slug>/', HotelDetailView.as_view(), name="hotel_detail")
 ]
