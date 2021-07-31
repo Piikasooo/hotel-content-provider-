f448fc4d0a82a7d772e930c68273149c7d943896
MKS <maks1111bm@gmail.com>
2021-07-31 22:42:12 +0300

    feat: add page CreateRoom, CreateCoef; modify CreateRoom (select from possible amenities, select from possible type_room)

--- Hotel_content_provider/urls.py
+++ Hotel_content_provider/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('enter/', include('hotelcontent.urls'))
+    path('', include('hotelcontent.urls'))
 ]
