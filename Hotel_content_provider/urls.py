f5ef46b7f4c2605a13e80e10a3b31236b9c2f853
EA2311 <andreyyerzhov@gmail.com>
2021-08-06 16:30:29 +0300

    cache

--- Hotel_content_provider/urls.py
+++ Hotel_content_provider/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('', include('hotelcontent.urls')),
 
     path('api/v1/', include('api.urls')),
+
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
