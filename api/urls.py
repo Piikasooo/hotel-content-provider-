35efefe6301778d5cf2c2cdd643eefbb847fbb74
MKS <maks1111bm@gmail.com>
2021-08-07 21:27:31 +0300

    feat: was made FilterDate for endpoint rooms/FilterDate, where also the price is calculated depending on the coefficients; was made endpoint /booking/ for book new rates

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HotelsView, RoomsHotelView, RoomsView, RoomsFilterDateView
+from .views import HotelsView, RoomsHotelView, RoomsView, RoomsFilterDateView, BookingView, MyBookingsView
 
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path('rooms/<slug:slug>/', RoomsHotelView.as_view()),
     path('rooms/', RoomsView.as_view()),
     path('rooms/FilterDate', RoomsFilterDateView.as_view()),
+    path('booking', BookingView.as_view()),
+    path('my_bookings/', MyBookingsView.as_view()),
 ]
