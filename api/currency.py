924bd1de7b73f1b5678ea963f2a483f81fab0311
MKS <maks1111bm@gmail.com>
2021-08-09 15:37:58 +0300

    feat: create function Currency for convert currency

--- api/currency.py
+++ api/currency.py
@@ -1,24 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
-
-UAH_USD = 'https://www.google.com/search?q=%D0%B3%D1%80%D0%B8%D0%B2%D0%BD%D0%B0+%D0%BA+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D1%83&sxsrf=ALeKk01HqsqbrdcNX0hqL3aDk-_zMJEV3w%3A1628240964618&ei=RPwMYf34JJ6A9u8PnKqD8AI&oq=%D0%B3%D1%80%D0%B8%D0%B2%D0%BD%D0%B0+%D0%BA+&gs_lcp=Cgdnd3Mtd2l6EAMYADILCAAQgAQQsQMQgwEyBQgAEMsBMgUIABCABDIFCAAQywEyBQgAEIAEMgUIABDLATIFCAAQgAQyBQgAEMsBMgUIABDLATIFCAAQywE6BwgjEOoCECc6BAgjECc6BAgAEEM6CwguEIAEEMcBEK8BOggIABCxAxCDAToKCAAQsQMQgwEQQzoICAAQgAQQsQM6BwgAELEDEENKBAhBGABQ_6QCWMq4AmC4yAJoAXACeACAAfEBiAHfC5IBBTAuOS4xmAEAoAEBsAEKwAEB&sclient=gws-wiz'
-UAH_EUR = 'https://www.google.com/search?q=%D0%B3%D1%80%D0%B8%D0%B2%D0%BD%D0%B0+%D0%BA+%D0%B5%D0%B2%D1%80%D0%BE&sxsrf=ALeKk00MXmbE8WaKg4mGRS5tu31qePM0BQ%3A1628243198340&ei=_gQNYdmGFI397_UPtLOk6AE&oq=%D0%B3%D1%80%D0%B8%D0%B2%D0%BD%D0%B0+%D0%BA+%D0%B5&gs_lcp=Cgdnd3Mtd2l6EAMYADIFCAAQgAQyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgcIABAKEMsBMgUIABDLATIFCAAQywEyCAgAEMkDEMsBMgUIABCABDoECAAQRzoECCMQJzoLCAAQgAQQsQMQgwE6EQguEIAEELEDEIMBEMcBENEDOgkIIxAnEEYQggI6BAgAEEM6CwguEIAEELEDEIMBOggILhCxAxCDAToICAAQsQMQgwE6CAguEIAEELEDSgQIQRgAUOIYWIU3YNVCaAFwA3gBgAGmAogBgg-SAQYwLjEwLjKYAQCgAQGwAQfIAQfAAQE&sclient=gws-wiz'
-
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
+from openexchangerate import OpenExchangeRates
 
 
-def convert_to(currency):
+client = OpenExchangeRates(api_key="6ff8323a7eed443693dd7d6bfbe2490b")
 
-    if currency == "USD":
-        UAH_USD_page = requests.get(UAH_USD, headers=headers)
-        soup_usd = BeautifulSoup(UAH_USD_page.content, 'html.parser')
-        convert = soup_usd.findAll("span", {"class": "DFlfde", "class": "SwHCTb", "data-precision": 3})
-        usd = convert[0].text
-        return usd
 
-    if currency == "EUR":
-        UAH_EUR_page = requests.get(UAH_EUR, headers=headers)
-        soup_eur = BeautifulSoup(UAH_EUR_page.content, 'html.parser')
-        convert = soup_eur.findAll("span", {"class": "DFlfde", "class": "SwHCTb", "data-precision": 3})
-        eur = float(convert[0].text)
-        return eur
+def currency(price_uah, request):
+    if not request.data.get("currency"):
+        uah = price_uah
+        return uah
+    elif request.data.get("currency") == "USD":
+        all_currency = client.latest()
+        uah = float(all_currency[0]['UAH'])
+        price_in_usd = float(price_uah) / uah
+        return price_in_usd
+    elif request.data.get("currency") == "EUR":
+        pass
+    else:
+        return price_uah
--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from hotelcontent.models import Hotel, Rooms, RateAmenity, Bookings, Coefficient, AgentReservation, User
 from .serializers import HotelsSerializer, RoomSerializer, RoomFilterSerializer, BookingSerializer
 from .functions import str_to_date, final_price_list, final_price
-from django.db.models import Q
+from django.db.models import Q, When
 from rest_framework import permissions
 
 
@@ -80,12 +80,30 @@ class RoomsFilterDateView(APIView):
     def post(self, request):
 
          start_date, end_date = str_to_date(request)
-
-         free_rooms = Rooms.objects.filter(
+         free_rooms = list(Rooms.objects.filter(
              Q(bookings=None) | (
-                     Q(bookings__checkin__gt=end_date) | Q(bookings__checkout__lt=start_date))
-         )
-         free_rooms = final_price_list(free_rooms=free_rooms, start_date=start_date, end_date=end_date, request=request)
+                     Q(bookings__checkin__gt=end_date) | Q(bookings__checkout__lt=start_date)
+             )
+         ))
+
+         rooms2 = list(Rooms.objects.exclude(
+             Q(bookings=None) or
+             (Q(bookings__checkin__gt=end_date) | Q(bookings__checkout__lt=start_date))
+         ))
+
+         print(free_rooms)
+         print(rooms2)
+
+         a = rooms2
+         b = free_rooms
+         for i in list(free_rooms):
+             if i in a:
+                 a.remove(i)
+                 b.remove(i)
+
+         print(a)
+         print(b)
+         free_rooms = final_price_list(free_rooms=b, start_date=start_date, end_date=end_date, request=request)
          serializer = RoomFilterSerializer(free_rooms, many=True)
          return Response({"rooms": serializer.data})
 
