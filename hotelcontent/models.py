e49cc54c6b7c6ff63880c92c0e91724fffc28c1c
EA2311 <andreyyerzhov@gmail.com>
2021-08-11 10:31:25 +0300

    Delete booking endpoint

--- hotelcontent/models.py
+++ hotelcontent/models.py
@@ -3,6 +3,8 @@ from datetime import date
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
+
+
 # from django_earthdistance.models import EarthDistanceQuerySet
 
 User = get_user_model()
@@ -65,7 +67,7 @@ class Rooms(models.Model):
     room_number = models.IntegerField()
     room_rate_price = models.DecimalField(max_digits=7, decimal_places=2, default=200.00)
 
-    # room_price = models.DecimalField(max_digits=7, decimal_places=2, default=200.00, blank=True)
+    room_price = models.DecimalField(max_digits=7, decimal_places=2, default=200.00, blank=True)
 
     def __str__(self):
         return '{}/{}'.format(self.hotel, self.room_number, self.room_type, self.room_rate_price)
@@ -76,13 +78,6 @@ class AgentReservation(models.Model):
 
     def __str__(self):
         return '{}'.format(self.agent)
-
-
-class BookingStatus(models.Model):
-    booking_status_description = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.booking_status_description
 
 
 class Bookings(models.Model):
@@ -114,7 +109,3 @@ class RateAmenity(models.Model):
 
     def __str__(self):
         return '{}'.format(self.amenity)
-
-
-
-
