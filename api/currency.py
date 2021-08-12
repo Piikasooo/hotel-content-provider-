26c5e5339afc737b456ced89aaac2bcfe294349d
MKS <maks1111bm@gmail.com>
2021-08-12 11:29:01 +0300

    fixes: the ability to take all currencies is available

--- api/currency.py
+++ api/currency.py
@@ -6,13 +6,16 @@ def convert_currency(price_uah, request):
     if not request.data.get("currency"):
         uah = price_uah
         return uah
-    elif request.data.get("currency") == "USD":
-        currency_name = "UAH"
-        date_need = datetime.date.today()
-        currency_usd_uah = get_currency(date_need, currency_name)
-        price_in_usd = float(price_uah) / currency_usd_uah
-        return price_in_usd
-    elif request.data.get("currency") == "EUR":
-        pass
     else:
-        return price_uah
+        currency_uah = "UAH"
+        date_need = datetime.date.today()
+        currency_usd_uah = get_currency(date_need, currency_uah)
+        price_in_usd = float(price_uah) / currency_usd_uah
+        if request.data.get("currency") == "USD":
+            return price_in_usd
+        else:
+            print("I am here")
+            currency_name = request.data.get("currency")
+            currency_name_usd = get_currency(date_need, currency_name)
+            price_in_name_currency = price_in_usd * currency_name_usd
+            return price_in_name_currency
