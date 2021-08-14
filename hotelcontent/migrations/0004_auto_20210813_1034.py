3b9db7bf68d286303153c93e4d7c1b2fc9ca6a1f
MKS <maks1111bm@gmail.com>
2021-08-14 14:51:51 +0300

    update file funtions.py - final_price_list, fixed model Rooms

--- hotelcontent/migrations/0004_auto_20210813_1034.py
+++ hotelcontent/migrations/0004_auto_20210813_1034.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('hotelcontent', '0003_alter_agentreservation_agent'),
+        ('hotelcontent', '0001_initial'),
     ]
 
     operations = [
