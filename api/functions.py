f5764052a299b92f2aa7b453245c24ffea3bad3d
MKS <maks1111bm@gmail.com>
2021-08-14 14:51:51 +0300

    update file funtions.py - final_price_list, fixed model Rooms

--- api/functions.py
+++ api/functions.py
@@ -47,32 +47,9 @@ def final_price_list(free_rooms, start_date, end_date, request):
     free_rooms = []
 
     for room in rooms:
-        st_date = start_date
-        total_price = 0.00
-        coefficients = Coefficient.objects.filter(hotel=Hotel.objects.get(hotel_name=room.hotel.hotel_name))
-
-        for coefficient in coefficients:
-
-            if (coefficient.start_date > end_date) | (coefficient.end_date < start_date):
-                room.room_price = 0
-                room.room_price = room.room_rate_price * int((end_date - start_date).days)
-                room.room_price = convert_currency(room.room_price, request)
-                room.save()
-                free_rooms.append(room)
-            else:
-                room.room_price = 0
-                while st_date != end_date:
-                    if coefficient.start_date <= st_date <= coefficient.end_date:
-                        total_price = (float(room.room_rate_price) * float(coefficient.coefficient)) + total_price
-                    else:
-                        total_price = total_price + float(room.room_rate_price)
-                    st_date = st_date + datetime.timedelta(days=1)
-                room.room_price = total_price
-                room.room_price = convert_currency(room.room_price, request)
-                room.save()
-                free_rooms.append(room)
+        room_append = final_price(room=room, start_date=start_date, end_date=end_date, request=request)
+        free_rooms.append(room_append)
     return free_rooms
-
 
 
 
