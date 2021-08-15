5f8f052b2e9c641af295cf3fd69f9057e1c5c6a1
EA2311 <andreyyerzhov@gmail.com>
2021-08-15 21:30:05 +0300

    Refactor imports, tebplates labels, delete useless classes, update hotel photo

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import HotelsView, RoomsHotelView, RoomsView, RoomsFilterDateView, BookingView, MyBookingsView, CancelBooking
+from .views import HotelsView, RoomsFilterDateView, BookingView, MyBookingsView, CancelBooking
 
 urlpatterns = [
     path('hotels/', HotelsView.as_view()),
-    path('rooms/<slug:slug>/', RoomsHotelView.as_view()),
-    path('rooms/', RoomsView.as_view()),
-    path('rooms/FilterDate', RoomsFilterDateView.as_view()),
+    path('rates', RoomsFilterDateView.as_view()),
     path('booking', BookingView.as_view()),
     path('my_bookings/', MyBookingsView.as_view()),
     path('my_bookings/cancel', CancelBooking.as_view()),
